b2c80614ad2a94629aa5699284880aa36ec73576
Arka Dutta <35682899+dutt-arka@users.noreply.github.com>
2021-10-23 10:01:55 +0530

    Live-User-Filter

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-const header = document.getElementById('header')
-const title = document.getElementById('title')
-const excerpt = document.getElementById('excerpt')
-const profile_img = document.getElementById('profile_img')
-const name = document.getElementById('name')
-const date = document.getElementById('date')
+const result = document.getElementById('result')
+const filter = document.getElementById('filter')
+const listItems = []
 
-const animated_bgs = document.querySelectorAll('.animated-bg')
-const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+getData()
 
-setTimeout(getData, 2500)
+filter.addEventListener('input', (e) => filterData(e.target.value))
 
-function getData() {
-  header.innerHTML =
-    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
-  title.innerHTML = 'Lorem ipsum dolor sit amet'
-  excerpt.innerHTML =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
-  profile_img.innerHTML =
-    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
-  name.innerHTML = 'John Doe'
-  date.innerHTML = 'Oct 08, 2020'
+async function getData() {
+    const res = await fetch('https://randomuser.me/api?results=50')
 
-  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
-  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+    const { results } = await res.json()
+
+    // Clear result
+    result.innerHTML = ''
+
+    results.forEach(user => {
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
+
+        result.appendChild(li)
+    })
+}
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
+    })
 }
